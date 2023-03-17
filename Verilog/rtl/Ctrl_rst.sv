72f11a79d70b38becc16baec56fcc2a6fc572a57
danhayoun <128132748+danhayoun@users.noreply.github.com>
2023-03-17 11:20:32 +0100

    Add files via upload

--- Verilog/rtl/Ctrl_rst.sv
+++ Verilog/rtl/Ctrl_rst.sv
@@ -6,11 +6,9 @@ module ctrl_rst (
     output logic req_out;
     output logic ack_in;
     output logic ctrl_out
-
-
 );
 
-Muller MG_rst_on(a.(req_in),b.(ack_out),rst.(rst),data_out.(req_out));
+Muller MG_rst_on(a.(req_in),b.(~ack_out),rst.(~rst),data_out.(req_out));
 assign ack_in = req_out;
 assign crtl_out = req_out;
 
