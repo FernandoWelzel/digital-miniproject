e6cf74e614041b59445f85ea1992b86a4a59ccd2
danhayoun <128132748+danhayoun@users.noreply.github.com>
2023-03-17 10:02:13 +0100

    Add files via upload

---
+++ Verilog/rtl/C-gate(rst 0).sv
@@ -0,0 +1,23 @@
+module Muller_C_Gate(req_in,ack_out,rst,req_out);
+input logic req_in;
+input logic ack_out;
+input logic rst;
+
+output logic req_out;
+
+logic req_out_m;
+
+assign req_out = req_in*(!ack_out) + (req_in + !ack_out)*
+
+
+always_ff@(req_out,rst)
+begin
+    if (rst) 
+    begin
+        req_out <= b'0;
+        req_out_m <= b'0;
+    else req_out_m <= req_out;
+
+end
+
+endmodule
