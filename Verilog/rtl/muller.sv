914944dfc19bcef42a54e443337df1fba493d7fc
FernandoWelzel <66530874+FernandoWelzel@users.noreply.github.com>
2023-03-17 14:43:08 +0100

    Verilog - Muller gate update
    Update after ModelSim

--- Verilog/rtl/muller.sv
+++ Verilog/rtl/muller.sv
@@ -4,22 +4,22 @@
 //  Description : Parametric muller gate (C element).
 //==============================================================================
 module muller #(
-  parameter integer       size = 2,  // Size - Number of inputs
+  parameter               size = 2   // Size - Number of inputs
   )(
-  input  logic   [size:0]  data_in,  // Input values
-  input  logic            data_out,  // Output value
-  input  logic                 rst   // Asyncronous posedge reset -> data_out = 0
+  input  logic [size-1:0]  data_in,  // Input values
+  input  logic                 rst,  // Asyncronous posedge reset -> data_out = 0
+  output logic            data_out   // Output value
 );
 
 // == Main Code ================================================================
-always_ff @ ( data_in, posedge rst ) begin
+always_ff @* begin
 
     if ( rst ) data_out <= 0;
     else begin
         if ( data_in == 0 ) begin
             data_out <= 0;    
         end
-        else ( & data_in ) begin
+        else if ( &data_in ) begin
             data_out <= 1;
         end
         else begin
