eb9a52e73263d973bafcfa7016462ceec46a4ff3
Fernando Welzel <fernando.welzel.br@gmail.com>
2023-04-06 23:38:37 +0200

    Verilog - Neuron revision

--- Verilog/neuron/neuron.sv
+++ Verilog/neuron/neuron.sv
@@ -4,56 +4,58 @@
 //  Description : One input spiking neuron desing to be used in neural networks.
 //
 //  Delay vector association (defined in ns)
-//   * delay_v[0] --> Weight multiplication
-//   * delay_v[1] --> Addition
-//   * delay_v[2] --> Comparison threshold
+//   * delay_v[0] --> Addition
+//   * delay_v[1] --> Comparison threshold
 //
 //==============================================================================
 module neuron #(
-  parameter integer               weight = 1,  // Weight to be applied to data_in
   parameter integer               thold  = 8,  // Threshold value
   parameter integer            data_bits = 4,  // Internal data resolution  
-  parameter integer delay_v [2:0] = {1, 1, 1}  // Delay vector from each combinational block - See description
+  parameter integer    delay_v [1:0] = {1, 1}  // Delay vector from each combinational block - See description
   )  
   (
-  input  logic    data_in, // Data from the previous neuron
-  input  logic     req_in, // Request from the previous neuron
-  output logic     ack_in, // Acknowledge to the previous neuron
+  input  logic [data_bits-1:0] data_in, // Data from the previous neuron
+  input  logic                  req_in, // Request from the previous neuron
+  output logic                  ack_in, // Acknowledge to the previous neuron
 
-  output logic   data_out, // Data to the next neuron
-  output logic    req_out, // Request to the next neuron
-  input  logic    ack_out, // Acknowledge to the next neuron
+  output logic                 req_out, // Request to the next neuron
+  input  logic                 ack_out, // Acknowledge to the next neuron
 
-  input  logic        rst  // Circuit asyncronous reset -> req_out = 0 
+  input  logic                     rst  // Circuit asyncronous reset -> req_out = 0 
 );
 
 timeunit      1ns;
 timeprecision 1ns;
 
 // == Variables Declaration ====================================================
-logic [5:0] ctrl_vec;
+logic [4:0] ctrl_vec;
+logic data_out_inter, req_out_inter, ack_out_inter; // Intermidiary variables to pass/dont-pass policy
 
-// == Blocks instantiation =====================================================
+// == Modules instantiation ====================================================
 neuron_ctrl #(
     .delay_v(delay_v)
 ) neuron_main_ctrl (
     .req_in(req_in),
     .ack_in(ack_in),
-    .req_out(req_out),
-    .ack_out(ack_out),
+    .req_out(req_out_inter),
+    .ack_out(ack_out_inter),
     .ctrl_vec(ctrl_vec),
     .rst(rst)
 );
 
 neuron_data #(
-    .weight(weight),
     .thold(thold),
     .data_bits(data_bits)
 ) neuron_main_data (
     .data_in(data_in),
-    .data_out(data_out),
+    .data_out(data_out_inter),
     .ctrl_vec(ctrl_vec),
     .rst(rst)
 );
+
+// == Blocks connections =====================================================
+// Implements pass/dont-pass policy -> I gave it this name, this name doesn't exist in the literature
+assign req_out = data_out_inter & req_out_inter;
+always @* #1 ack_out_inter = (~data_out_inter & req_out_inter) | ack_out; // Small delay for system stability
 
 endmodule
--- Verilog/neuron/neuron_ctrl.sv
+++ Verilog/neuron/neuron_ctrl.sv
@@ -4,13 +4,12 @@
 //  Description : Control block for spiking neuron.
 //
 //  Delay vector association (defined in ns)
-//   * delay_v[0] --> Weight multiplication
-//   * delay_v[1] --> Addition
-//   * delay_v[2] --> Comparison threshold
+//   * delay_v[0] --> Addition
+//   * delay_v[1] --> Comparison threshold
 //
 //==============================================================================
 module neuron_ctrl #(
-  parameter integer delay_v [2:0] = {1, 1, 1}  // Delay vector from each combinational block - See description
+  parameter integer delay_v [1:0] = {1, 1}  // Delay vector from each combinational block - See description
   )(
   input  logic         req_in, // Request from the previous neuron
   output logic         ack_in, // Acknowledge to the previous neuron
@@ -18,7 +17,7 @@ module neuron_ctrl #(
   output logic        req_out, // Request to the next neuron
   input  logic        ack_out, // Acknowledge to the next neuron
 
-  output logic [5:0] ctrl_vec, // Control vector
+  output logic [4:0] ctrl_vec, // Control vector
 
   input  logic            rst  // Circuit asyncronous reset -> req_out = 0 
 );
@@ -32,7 +31,6 @@ logic  req_in_ctrl_2, req_out_ctrl_2,  ack_in_ctrl_2, ack_out_ctrl_2;
 logic  req_in_ctrl_3, req_out_ctrl_3,  ack_in_ctrl_3, ack_out_ctrl_3;
 logic  req_in_ctrl_4, req_out_ctrl_4,  ack_in_ctrl_4, ack_out_ctrl_4;
 logic  req_in_ctrl_5, req_out_ctrl_5,  ack_in_ctrl_5, ack_out_ctrl_5;
-logic  req_in_ctrl_6, req_out_ctrl_6,  ack_in_ctrl_6, ack_out_ctrl_6;
 
 logic  [1:0]  req_in_join,   ack_in_join;
 logic        req_out_join,  ack_out_join;
@@ -41,13 +39,12 @@ logic         req_in_fork,   ack_in_fork;
 logic  [1:0] req_out_fork,  ack_out_fork;
 
 // == Blocks instantiation =====================================================
-ctrl ctrl_1(.req_in(req_in_ctrl_1), .req_out(req_out_ctrl_1), .ack_in(ack_in_ctrl_1), .ack_out(ack_out_ctrl_1), .ctrl_out(ctrl_vec[0]), .rst(rst));
+ctrl ctrl_2(.req_in(req_in_ctrl_2), .req_out(req_out_ctrl_2), .ack_in(ack_in_ctrl_2), .ack_out(ack_out_ctrl_2), .ctrl_out(ctrl_vec[1]), .rst(rst));
 ctrl ctrl_3(.req_in(req_in_ctrl_3), .req_out(req_out_ctrl_3), .ack_in(ack_in_ctrl_3), .ack_out(ack_out_ctrl_3), .ctrl_out(ctrl_vec[2]), .rst(rst));
 ctrl ctrl_4(.req_in(req_in_ctrl_4), .req_out(req_out_ctrl_4), .ack_in(ack_in_ctrl_4), .ack_out(ack_out_ctrl_4), .ctrl_out(ctrl_vec[3]), .rst(rst));
 ctrl ctrl_5(.req_in(req_in_ctrl_5), .req_out(req_out_ctrl_5), .ack_in(ack_in_ctrl_5), .ack_out(ack_out_ctrl_5), .ctrl_out(ctrl_vec[4]), .rst(rst));
-ctrl ctrl_6(.req_in(req_in_ctrl_6), .req_out(req_out_ctrl_6), .ack_in(ack_in_ctrl_6), .ack_out(ack_out_ctrl_6), .ctrl_out(ctrl_vec[5]), .rst(rst));
 
-ctrl_rst_on ctrl_2(.req_in(req_in_ctrl_2), .req_out(req_out_ctrl_2), .ack_in(ack_in_ctrl_2), .ack_out(ack_out_ctrl_2), .ctrl_out(ctrl_vec[1]), .rst(rst));
+ctrl_rst_on ctrl_1(.req_in(req_in_ctrl_1), .req_out(req_out_ctrl_1), .ack_in(ack_in_ctrl_1), .ack_out(ack_out_ctrl_1), .ctrl_out(ctrl_vec[0]), .rst(rst));
 
 join_param #(.size(2)) join_main(
   .req_in(req_in_join),
@@ -66,38 +63,32 @@ fork_param #(.size(2)) fork_main(
 );
 
 // == Connecting with delay =================================================
-always @(req_out_ctrl_1) begin
-  #(delay_v[0]) req_in_ctrl_3 <= req_out_ctrl_1;
-end
-
 always @(req_out_join) begin
-  #(delay_v[1]) req_in_ctrl_4 <= req_out_join;
+  #(delay_v[0]) req_in_ctrl_3 <= req_out_join;
 end
 
-always @(req_out_ctrl_4) begin
-  #(delay_v[2]) req_in_fork <= req_out_ctrl_4;
+always @(req_out_ctrl_3) begin
+  #(delay_v[1]) req_in_fork <= req_out_ctrl_3;
 end
 
 // == Connecting blocks =====================================================
-assign req_in_ctrl_1 = req_in;
-assign ack_in = ack_in_ctrl_1;
+assign req_in_ctrl_2 = req_in;
+assign ack_in = ack_in_ctrl_2;
 
-assign ack_out_ctrl_1 = ack_in_ctrl_3;
+assign req_in_join = {req_out_ctrl_2, req_out_ctrl_1};
+assign ack_out_ctrl_1 = ack_in_join[0];
+assign ack_out_ctrl_2 = ack_in_join[1];
+assign ack_out_join = ack_in_ctrl_3;
 
-assign req_in_join = {req_out_ctrl_2, req_out_ctrl_3};
-assign ack_out_ctrl_2 = ack_in_join[0];
-assign ack_out_ctrl_3 = ack_in_join[1];
-assign ack_out_join = ack_in_ctrl_4;
+assign ack_out_ctrl_3 = ack_in_fork;
+assign req_in_ctrl_4 = req_out_fork[0];
+assign req_in_ctrl_5 = req_out_fork[1];
+assign ack_out_fork = {ack_in_ctrl_5, ack_in_ctrl_4}; 
 
-assign ack_out_ctrl_4 = ack_in_fork;
-assign req_in_ctrl_5 = req_out_fork[0];
-assign req_in_ctrl_6 = req_out_fork[1];
-assign ack_out_fork = {ack_in_ctrl_5, ack_in_ctrl_6}; 
+assign req_in_ctrl_1 = req_out_ctrl_4;
+assign ack_out_ctrl_4 = ack_in_ctrl_1;
 
-assign req_in_ctrl_2 = req_out_ctrl_5;
-assign ack_out_ctrl_5 = ack_in_ctrl_2;
-
-assign req_out = req_out_ctrl_6;
-assign ack_out_ctrl_6 = ack_out;
+assign req_out = req_out_ctrl_5;
+assign ack_out_ctrl_5 = ack_out;
 
 endmodule
